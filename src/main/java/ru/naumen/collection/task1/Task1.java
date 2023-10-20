ce08691f8c9686a8c4c7be1601c08bb1e2282e95
Ivan Kropotin <kropotiniv@yandex.ru>
2023-10-20 12:17:26 +0500

    Задача 1

--- src/main/java/ru/naumen/collection/task1/Task1.java
+++ src/main/java/ru/naumen/collection/task1/Task1.java
@@ -1,9 +1,6 @@
 package ru.naumen.collection.task1;
 
-import ru.naumen.collection.task2.Ticket;
-
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Дано:
@@ -25,7 +22,7 @@ import java.util.List;
  * <p>Метод должен быть оптимален по производительности.</p>
  * <p>Пользоваться можно только стандартными классами Java SE.
  * Коллекции collA, collB изменять запрещено.</p>
- *
+ * <p>
  * См. {@link User}
  *
  * @author vpyzhyanov
@@ -33,11 +30,29 @@ import java.util.List;
  */
 public class Task1 {
 
+    public static void main(String[] args) {
+        Collection<User> firstCol = List.of(new User("1", "1@mail.com", new byte[]{0, 0, 0}));
+        Collection<User> secondCol = List.of(new User("1", "1@mail.com", new byte[]{0, 0, 0}));
+        List<User> duplicates = findDuplicates(firstCol, secondCol);
+        duplicates.forEach(System.out::println);
+    }
+
     /**
      * Возвращает дубликаты пользователей, которые есть в обеих коллекциях
      */
     public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB) {
-        // TODO
-        return null;
+        /* Используем HashSet, так как он позволяет проверять наличие элемента за константное время
+        Будет выполнено порядка n вставок в множество, где n - количество элементов в коллекции collB
+        (при этом операция вставки тоже константная) */
+        Set<User> collBSet = new HashSet<>(collB);
+        List<User> duplicates = new ArrayList<>();
+        // Будет совершено порядка m операций, где m - количество элементов в коллекции collA
+        for (User user : collA) {
+            if (collBSet.contains(user)) {
+                duplicates.add(user);
+            }
+        }
+        return duplicates;
+        // Итоговая сложность O(n), где n - количество элементов в первой и второй коллекциях
     }
 }
--- src/main/java/ru/naumen/collection/task1/User.java
+++ src/main/java/ru/naumen/collection/task1/User.java
@@ -1,5 +1,8 @@
 package ru.naumen.collection.task1;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Пользователь
  *
@@ -10,4 +13,36 @@ public class User {
     private String username;
     private String email;
     private byte[] passwordHash;
+
+    public User(String username, String email, byte[] passwordHash) {
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username)
+                && Objects.equals(email, user.email)
+                && Arrays.equals(passwordHash, user.passwordHash);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(username, email);
+        result = 31 * result + Arrays.hashCode(passwordHash);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", passwordHash=" + Arrays.toString(passwordHash) +
+                '}';
+    }
 }
